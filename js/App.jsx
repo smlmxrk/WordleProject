a302cc6dd886594167d97b5e30cce5c5c4847788
sml.mxrk <marktheartest@gmail.com>
2025-03-26 16:24:41 -0500

    started refactoring into a react project

---
+++ js/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Game Selection</h1>
+      <p>Choose a game to play!</p>
+    </div>
+  );
+};
+
+export default App;
